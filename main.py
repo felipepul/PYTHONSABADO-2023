997903feee5a92488aeb48d3afabe5c7bfe42a8a
cuatrotales <jfquinterogu14@gmail.com>
2023-02-18 12:05:21 -0500

    NT2 se crea archivo main y se crera casteo de variable

---
+++ main.py
@@ -0,0 +1,6 @@
+numeroUno = int(input('digite numero uno'))
+numeroDos = int(input('digite numero dos'))
+print(f'el numero uno es:{numeroUno}')
+print(f'el numero Dos es:{numeroDos}')
+suma = numeroDos + numeroDos
+print(f'la suma total es:{suma}')
